10220229164527b1c93798d8e6e84e1ab5d67378
Mido <elm042025@gmail.com>
2025-08-14 14:28:02 +0200

    Enhance HTML structure, import CSS modules for styling, and update favicon; refactor Dish and Meny components for improved layout and responsiveness

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,7 +1,9 @@
+import styles from "./Dish.module.css";
+
 export const Dish = function ({ kategori, tittel, pris, ingredienser }) {
    return (
       <article>
-         <section className="dish-header-price">
+         <section className={styles.dishHeaderPrice}>
             <h3>{tittel}</h3>
             <span>{pris}</span>
          </section>
--- src/components/Meny.jsx
+++ src/components/Meny.jsx
@@ -1,13 +1,15 @@
 import { Dish } from "./Dish.jsx";
+import styles from "./Meny.module.css"; //! imports the css module for styling
 
 export const Meny = function ({ menyData = [] }) {
    const categories = ["Forrett", "Hovedrett", "Dessert"];
    return (
-      <section className="menu-grid">
+      <section className={styles.menuGrid}>
+         <h1>MAT MENY</h1>
          {categories.map((kategori) => (
             <section
                key={kategori}
-               className="menu-category"
+               className={styles.menuCategory}
             >
                <h2>{kategori}</h2>
                {menyData
